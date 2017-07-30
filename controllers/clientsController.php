328896e3129a1d73d89487be420de0beb1e1ea5a
conejofoz <silviocoelho.udc@gmail.com>
2017-07-29 23:10:05 -0300

    aula 19 ok

--- controllers/clientsController.php
+++ controllers/clientsController.php
@@ -41,10 +41,8 @@ class clientsController extends controller {
         $company = new Companies($u->getCompany());
         $data['company_name'] = $company->getName();
         $data['user_email'] = $u->getEmail();
-
         if ($u->hasPermission('clients_edit')) {
             $c = new Clients();
-            
             if(isset($_POST['name']) && !empty($_POST['name'])){
                 $name = addslashes($_POST['name']);
                 $email = addslashes($_POST['email']);
@@ -59,16 +57,60 @@ class clientsController extends controller {
                 $address_city = addslashes($_POST['address_city']);
                 $address_state = addslashes($_POST['address_state']);
                 $address_country = addslashes($_POST['address_country']);
-                
                 $c->add($u->getCompany(), $name, $email, $phone, $stars, $internal_obs,
                         $address_zipcode, $address, $address_number, $address2, $address_neighb, $address_city,
                         $address_state, $address_country);
                 header("Location: " . BASE_URL."/clients");
             }
-
             $this->loadTemplate('clients_add', $data);
         } else {
             header("Location: " . BASE_URL."/clients");
         }
     }
+    
+    
+    
+        public function edit($id){
+        $data = array();
+        $u = new Users();
+        $u->setLoggedUser();
+        $company = new Companies($u->getCompany());
+        $data['company_name'] = $company->getName();
+        $data['user_email'] = $u->getEmail();
+        if ($u->hasPermission('clients_edit')) {
+            $c = new Clients();
+            if(isset($_POST['name']) && !empty($_POST['name'])){
+                $name = addslashes($_POST['name']);
+                $email = addslashes($_POST['email']);
+                $phone = addslashes($_POST['phone']);
+                $stars = addslashes($_POST['stars']);
+                $internal_obs = addslashes($_POST['internal_obs']);
+                $address_zipcode = addslashes($_POST['address_zipcode']);
+                $address = addslashes($_POST['address']);
+                $address_number = addslashes($_POST['address_number']);
+                $address2 = addslashes($_POST['address2']);
+                $address_neighb = addslashes($_POST['address_neighb']);
+                $address_city = addslashes($_POST['address_city']);
+                $address_state = addslashes($_POST['address_state']);
+                $address_country = addslashes($_POST['address_country']);
+                $c->edit($id, $u->getCompany(), $name, $email, $phone, $stars, $internal_obs,
+                        $address_zipcode, $address, $address_number, $address2, $address_neighb, $address_city,
+                        $address_state, $address_country);
+                header("Location: " . BASE_URL."/clients");
+            }
+            
+            $data['client_info'] = $c->getInfo($id, $u->getCompany());
+            
+            $this->loadTemplate('clients_edit', $data);
+        } else {
+            header("Location: " . BASE_URL."/clients");
+        }
+    }
+    
+    
+    
+    public function delete($id){
+        echo "falta fazer este método";
+    }
+
 }
