953b5b6ce37652b2bdfc2d078324f4ec792c46ab
conejofoz <silviocoelho.udc@gmail.com>
2017-08-15 00:52:47 -0300

    aula 28 OK

--- views/sales_add.php
+++ views/sales_add.php
@@ -1,1 +1,9 @@
 <h1>Vendas - Adicionar</h1>
+<form method="POST">
+    <label for="client_name">Nome do Cliente</label><br/>
+    <input type="text" name="client_name" id="client_name" data-type="search_clients"/>
+    <button class="client_add_button">+</button>
+    <br/><br/>
+        
+</form>
+<script type="text/javascript" src="<?php echo BASE_URL; ?>/assets/js/script_sales_add.js"></script>    
